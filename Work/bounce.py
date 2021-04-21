86ab401dc7aebcb279fa98ba00e4e70aeb3112c1
Henry <ascencio.henan@gmail.com>
2021-04-21 11:11:51 -0600

    solve exercise 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+initial_height = 100
+bounce_backup_height = 3/5
+number_of_bounces = 10
+
+for bounce in range(number_of_bounces):
+    bounce_number = bounce+1
+    height = round(initial_height*bounce_backup_height, 4)
+    initial_height = height
+    print(bounce_number, height)
