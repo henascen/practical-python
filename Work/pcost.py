50259cd085899a4c39962aef3aaae7ef161ff9c8
Henry <ascencio.henan@gmail.com>
2021-04-28 14:08:09 -0600

    adding excersise to handle files

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,11 @@
 # pcost.py
 #
 # Exercise 1.27
+purchase_prices = []
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        row = line.rstrip("\n").split(',')
+        purchase_prices.append(float(row[2])*int(row[1]))
+
+print("Total cost ", sum(purchase_prices))
